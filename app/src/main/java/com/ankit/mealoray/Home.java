6c090a500359b2bc7f7a9593b3a9bbb301343498
Ankit Chaudhary <my007id@gmail.com>
2017-05-04 19:28:40 +0200

    added code for login and signup

--- app/src/main/java/com/ankit/mealoray/Home.java
+++ app/src/main/java/com/ankit/mealoray/Home.java
@@ -1,5 +1,6 @@
 package com.ankit.mealoray;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +14,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.ankit.mealoray.activities.LoginActivity;
+import com.ankit.mealoray.activities.SignupActivity;
+
 public class Home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -23,10 +27,15 @@ public class Home extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        FloatingActionButton createGroup = (FloatingActionButton) findViewById(R.id.create_group);
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
+        createGroup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
@@ -34,7 +43,7 @@ public class Home extends AppCompatActivity
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
